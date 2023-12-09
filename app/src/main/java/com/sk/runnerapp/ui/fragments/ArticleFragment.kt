81d4e0a7f541602c24536be843af3949d371f99d
Sandeep <sandeepsk15892>
2023-12-09 15:29:46 +0530

    code

--- app/src/main/java/com/sk/runnerapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/sk/runnerapp/ui/fragments/ArticleFragment.kt
@@ -1,18 +1,8 @@
 package com.sk.runnerapp.ui.fragments
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sk.runnerapp.R
 
-class ArticleFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
-    }
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
 }
