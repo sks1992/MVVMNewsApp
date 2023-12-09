30a51dba213da9b8843d192a3086c4d38243b8f2
Sandeep <sandeepsk15892>
2023-12-09 14:36:45 +0530

    code

---
+++ app/src/main/java/com/sk/runnerapp/ui/viewModel/NewsViewMode.kt
@@ -0,0 +1,94 @@
+package com.sk.runnerapp.ui.viewModel
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.sk.runnerapp.model.Article
+import com.sk.runnerapp.model.NewsResponse
+import com.sk.runnerapp.repository.NewsRepository
+import com.sk.runnerapp.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class NewsViewMode(app: Application, val newsRepository: NewsRepository) : AndroidViewModel(app) {
+    val headlines: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var headlinesPage = 1
+    var headlinesResponse: NewsResponse? = null
+
+    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
+    var searchNewsResponse: NewsResponse? = null
+    var newsSearchQuery: String? = null
+    var oldSearchQuery: String? = null
+
+    private fun handleHeadlinesResponse(response: Response<NewsResponse>):
+            Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                headlinesPage++
+                if (headlinesResponse == null) {
+                    headlinesResponse = resultResponse
+                } else {
+                    val oldArticle = headlinesResponse?.articles
+                    val newArticle = resultResponse.articles
+                    oldArticle?.addAll(newArticle)
+                }
+                return Resource.Success(headlinesResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                headlinesPage++
+                if (searchNewsResponse == null || newsSearchQuery != oldSearchQuery) {
+                    searchNewsPage = 1
+                    oldSearchQuery = newsSearchQuery
+                    searchNewsResponse = resultResponse
+                } else {
+                    searchNewsPage++
+                    val oldArticle = searchNewsResponse?.articles
+                    val newArticle = resultResponse.articles
+                    oldArticle?.addAll(newArticle)
+                }
+                return Resource.Success(searchNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+
+    fun addToFavorites(article: Article) = viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getFavouritesNews() = newsRepository.getFavouriteNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepository.deleteArticle(article)
+    }
+
+    fun internetConnection(context: Context): Boolean {
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            return getNetworkCapabilities(activeNetwork)?.run {
+                when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            } ?: false
+        }
+    }
+
+    private suspend fun headlinesInternet(countryCode: String) {
+        headlines.postValue(Resource.Loading())
+
+    }
+}
