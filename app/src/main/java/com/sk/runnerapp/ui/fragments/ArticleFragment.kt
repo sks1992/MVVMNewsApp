86304355e5e033e7e297b90456fe79472f01bed5
Sandeep <sandeepsk15892>
2023-12-11 09:40:27 +0530

    code

--- app/src/main/java/com/sk/runnerapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/sk/runnerapp/ui/fragments/ArticleFragment.kt
@@ -1,8 +1,38 @@
 package com.sk.runnerapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.sk.runnerapp.R
+import androidx.navigation.fragment.navArgs
+import com.google.android.material.snackbar.Snackbar
+import com.sk.runnerapp.databinding.FragmentArticleBinding
+import com.sk.runnerapp.ui.activity.NewsActivity
+import com.sk.runnerapp.ui.viewModel.NewsViewMode
 
-class ArticleFragment : Fragment(R.layout.fragment_article) {
+class ArticleFragment : Fragment(/*R.layout.fragment_article*/) {
+    lateinit var newsViewMode: NewsViewMode
+    val args: ArticleFragmentArgs by navArgs()
+    lateinit var binding: FragmentArticleBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentArticleBinding.bind(view)
+
+        newsViewMode = (activity as NewsActivity).newsViewMode
+        val article = args.article
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            article.url?.let {
+                loadUrl(it)
+            }
+        }
+
+        binding.fab.setOnClickListener {
+            newsViewMode.addToFavorites(article)
+            Snackbar.make(view, "Add to favourites", Toast.LENGTH_SHORT).show()
+
+        }
+    }
 }
--- app/src/main/java/com/sk/runnerapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/sk/runnerapp/ui/fragments/HeadlinesFragment.kt
@@ -1,9 +1,31 @@
 package com.sk.runnerapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.sk.runnerapp.R
+import com.sk.runnerapp.adapters.NewsAdapter
+import com.sk.runnerapp.ui.viewModel.NewsViewMode
 
 
 class HeadlinesFragment : Fragment(R.layout.fragment_headlines) {
+    lateinit var newsViewModel: NewsViewMode
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryButton: Button
+    lateinit var errorTextView: TextView
+    lateinit var itemHeadlinesError: CardView
 
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
